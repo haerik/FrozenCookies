60447ba1dc313e7d93aa1ba1b2b6a693bc854449
haerik <erik.g.hasse@gmail.com>
2017-09-01 23:20:44 -0500

    Update fc_bookmarklet_loader.js

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -2,8 +2,8 @@ javascript:(
   function () {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('id', 'frozenCookieScript');
-    js.setAttribute('src', 'https://rawgithub.com/haerik/FrozenCookies/master/frozen_cookies.js');
+    js.setAttribute('id', 'SpellAutomationScript');
+    js.setAttribute('src', 'https://rawgithub.com/haerik/FrozenCookies/SpellAutomation/frozen_cookies.js');
     document.head.appendChild(js);
  }()
 );
